158ada009c94a80d70c614a1732232fc93c46326
钟洋洋 <1768696618@qq.com>
2017-04-24 10:03:49 +0800

    Initial Commit

---
+++ bisaijifen2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bisaijifen2
+//
+//  Created by 20151104697 on 17/4/24.
+//  Copyright © 2017年 20151104697. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
